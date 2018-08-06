94fb7f4de2925f0771144fe5cca768074e8adc20
Vadim Melnikov <Vadimvadic_m@mail.ru>
2018-08-06 18:29:21 +0300

    Binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,12 +3,34 @@ import {getFunName} from "../helpers";
 
 
 class StorePicker extends React.Component {
+
+
+    myInput = React.createRef();
+
+    goToStore = event => {
+        // 1. Stop default submiting
+        event.preventDefault();
+        // 2. Get text from the input
+
+        // 3. Change the page to /store/whatever-asdasd
+        console.log(this.setState);
+    }
+    componentDidMount() {
+        console.log('Mounted');
+        console.log(this);
+    }
     render() {
         return (
             <Fragment>
-                <form action="" className="store-selector">
+                <form action="" className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please Enter a store</h2>
-                    <input type="text" required placeholder="Store Name" value={getFunName()}/>
+                    <input
+                        type="text"
+                        required
+                        placeholder="Store Name"
+                        value={getFunName()}
+                        ref={this.myInput}
+                    />
                     <button type="submit">Visit Store</button>
                 </form>
             </Fragment>
